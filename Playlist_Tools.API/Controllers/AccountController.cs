2d91233925b6d5c1f1be04e047e1c7aee834d7bc
yazstonn <yazstonn@hotmail.fr>
2025-05-07 15:22:55 +0200

    routing auth

---
+++ Playlist_Tools.API/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿// Dans le dossier Playlist_Tools.API/Controllers
+using Microsoft.AspNetCore.Mvc;
+using Playlist_tools.Application.Abstracts;
+using Playlist_Tools.Domain.Requests;
+
+namespace Playlist_Tools.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterRequest registerRequest)
+        {
+            await _accountService.RegisterUserAsync(registerRequest);
+            return Ok();
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginRequest loginRequest)
+        {
+            await _accountService.LoginAsync(loginRequest);
+            return Ok();
+        }
+
+        [HttpPost("refresh")]
+        public async Task<IActionResult> Refresh()
+        {
+            var refreshToken = Request.Cookies["REFRESH_TOKEN"];
+            await _accountService.RefreshTokenAsync(refreshToken);
+            return Ok();
+        }
+    }
+}
---
+++ Playlist_Tools.API/Controllers/MoviesController.cs
@@ -0,0 +1,18 @@
+﻿// Dans le dossier Playlist_Tools.API/Controllers
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Playlist_Tools.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MoviesController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public IActionResult GetMovies()
+        {
+            return Ok(new List<string>{"Matrix"});
+        }
+    }
+}
